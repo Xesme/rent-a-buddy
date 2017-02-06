63bb1e49808755899a08ce714e4ce429c9fdbe49
Jennifer Beem and Xia Amendolara <xia@>
2017-02-06 15:49:38 -0800

    add folders app, web, and src set up index.php, app.php, and src/rentables.php

---
+++ src/rentables.php
@@ -0,0 +1,24 @@
+<?php
+    class kitten
+    {
+        public $name;
+        public $age;
+        public $weight;
+        public $color;
+        public $gender;
+
+        function __construct($name, $age, $weight, $color, $gender)
+        {
+            $this->name = $name;
+            $this->age = $age;
+            $this->weight = $weight;
+            $this->color = $color;
+            $this->gender = $gender;  
+        }
+    }
+
+
+
+
+
+?>
