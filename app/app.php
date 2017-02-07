a790b12e55d5a881e0b13b8b75854545a4986569
Jennifer Beem and Xia Amendolara <jennifer@>
2017-02-07 10:48:50 -0800

    add output

--- app/app.php
+++ app/app.php
@@ -1,10 +1,66 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/rentables.php";
 
     $app = new Silex\Application();
 
     $app->get("/", function() {
-        return "Home";
+            return "
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
+                <title>Rent a Cat!</title>
+            </head>
+            <body>
+                <div class='container'>
+                    <h1>Rent a Cat</h1>
+                    <p>Enter the max weight and age of the cat you want to rent</p>
+                    <form action='/view_cats'>
+                        <div class='form-group'>
+                          <label for='age'>Enter the max age:</label>
+                          <input id='age' name='age' class='form-control' type='number'>
+                        </div>
+                        <div class='form-group'>
+                          <label for='weight'>Enter the max. weight (in pounds):</label>
+                          <input id='weight' name='weight' class='form-control' type='number'>
+                        </div>
+                        <button type='submit' class='btn-success'>Submit</button>
+                    </form>
+                </div>
+            </body>
+            </html>
+            ";
+        });
+
+    $app->get("/view_cats", function() {
+      $skinny = new Cat("skinny", 8, 30, "white and grey", "female");
+      $spongeBob = new Cat("Sponge Bob", 13, 35, "orange", "male");
+      $meow = new Cat("Meow", 3, 43, "black", "male");
+
+      $cats = array($skinny, $spongeBob, $meow);
+
+      $cats_matching_search = array();
+      foreach ($cats as $cat) {
+        if ($cat->age < $_GET["age"] && $cat->weight < $_GET["weight"]) {
+          array_push($cats_matching_search, $cat);
+        }
+      }
+
+      $output = "";
+      foreach ($cats_matching_search as $cat) {
+                $output = $output . "
+                <li> $cat->name </li>
+                <ul>
+                <li> age: $cat->age </li>
+                <li> weight: $cat->weight lbs</li>
+                <li> gender: $cat->gender </li>
+                </ul>
+                </body>
+                </html>
+                ";
+            }
+            return $output;
     });
 
     return $app;
--- src/rentables.php
+++ src/rentables.php
@@ -7,36 +7,24 @@
         public $color;
         public $gender;
 
-        function __construct($name, $age, $weight, $color, $gender, $image)
+        function __construct($name, $age, $weight, $color, $gender)
         {
             $this->name = $name;
             $this->age = $age;
             $this->weight = $weight;
             $this->color = $color;
             $this->gender = $gender;
-            $this->image = $image;
+            // $this->image = $image;
         }
 
-        function setWeight =($new_weight);
-        {
-            $float_weight = (float) $new_weight;
-            if ($float_weight !=0) {
-                $formatted_weight = number_format($float_weight, 0);
-                $this->weight = $formmated_weight;
-            }
-        }
-    }
-
-    $skinny = new Cat("skinny", 8, 30.52, "white and grey", "female", "img/skinny.jpg");
-    $spongeBob = new Cat("Sponge Bob", 13, 35.08, "orange", "male", "img/sponge-bob.jpg");
-    $meow = new Cat("Meow", 3, 43.22, "black", "male", "img/meow.jpg");
-
-    $cats = array($skinny, $spongeBob, $meow);
-
-    $cats_matching_search = array();
-    foreach ($cats as $cat) {
-        if ($cat->age < $_GET["age"] && $cat->weight < $_GET["weight"]) {
-            array_push($cats_matching_search, $cat);
-        }
+        // function setWeight = ($new_weight);
+        // {
+        //     $float_weight = (float) $new_weight;
+        //     if ($float_weight !=0) {
+        //         $formatted_weight = number_format($float_weight, 0);
+        //         $this->weight = $formmated_weight;
+        //     }
+        // }
+    // }
     }
 ?>
