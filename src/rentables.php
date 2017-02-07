cbedbc0a1d1e55b1cb850d2144328c7e6ab4e67f
Jennifer Beem and Xia Amendolara <jennifer@>
2017-02-07 09:16:17 -0800

    add cat object information and foreach loop

--- src/rentables.php
+++ src/rentables.php
@@ -1,5 +1,5 @@
 <?php
-    class kitten
+    class Cat
     {
         public $name;
         public $age;
@@ -13,8 +13,22 @@
             $this->age = $age;
             $this->weight = $weight;
             $this->color = $color;
-            $this->gender = $gender;  
+            $this->gender = $gender;
         }
+
+        $skinny = new Cat("skinny", 8, 30.52, "white and grey", "female");
+        $spongeBob = new Cat("Sponge Bob", 13, 35.08, "orange", "male");
+        $meow = new Cat("Meow", 3, 43.22, "black", "male");
+
+        $cats = array($skinny, $spongeBob, $meow);
+
+        $cats_matching_search = array();
+        foreach ($cats as $cat) {
+            if ($cat->age < $_GET["age"] && $cat->weight < $_GET["weight"]) {
+              array_push($cats_matching_search, $cat);
+            }
+        }
+
     }
 
 
