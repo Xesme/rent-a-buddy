a9e7fa7d1a51fa580ca539b2d8440ec9a57a51ca
Jennifer Beem and Xia Amendolara <xia@>
2017-02-07 09:24:46 -0800

    add setWeight method

--- src/rentables.php
+++ src/rentables.php
@@ -7,32 +7,36 @@
         public $color;
         public $gender;
 
-        function __construct($name, $age, $weight, $color, $gender)
+        function __construct($name, $age, $weight, $color, $gender, $image)
         {
             $this->name = $name;
             $this->age = $age;
             $this->weight = $weight;
             $this->color = $color;
             $this->gender = $gender;
+            $this->image = $image;
         }
 
-        $skinny = new Cat("skinny", 8, 30.52, "white and grey", "female");
-        $spongeBob = new Cat("Sponge Bob", 13, 35.08, "orange", "male");
-        $meow = new Cat("Meow", 3, 43.22, "black", "male");
-
-        $cats = array($skinny, $spongeBob, $meow);
-
-        $cats_matching_search = array();
-        foreach ($cats as $cat) {
-            if ($cat->age < $_GET["age"] && $cat->weight < $_GET["weight"]) {
-              array_push($cats_matching_search, $cat);
+        function setWeight =($new_weight);
+        {
+            $float_weight = (float) $new_weight;
+            if ($float_weight !=0) {
+                $formatted_weight = number_format($float_weight, 0);
+                $this->weight = $formmated_weight;
             }
         }
-
     }
 
+    $skinny = new Cat("skinny", 8, 30.52, "white and grey", "female", "img/skinny.jpg");
+    $spongeBob = new Cat("Sponge Bob", 13, 35.08, "orange", "male", "img/sponge-bob.jpg");
+    $meow = new Cat("Meow", 3, 43.22, "black", "male", "img/meow.jpg");
 
+    $cats = array($skinny, $spongeBob, $meow);
 
-
-
+    $cats_matching_search = array();
+    foreach ($cats as $cat) {
+        if ($cat->age < $_GET["age"] && $cat->weight < $_GET["weight"]) {
+            array_push($cats_matching_search, $cat);
+        }
+    }
 ?>
